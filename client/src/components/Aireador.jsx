d76c2940c3b639a2b6d69a9e4c261a0107921f5e
Ricardo Fernandez <rckfr.fr@hotmail.com>
2023-09-24 23:33:09 -0300

    airedor

--- client/src/components/Aireador.jsx
+++ client/src/components/Aireador.jsx
@@ -2,7 +2,7 @@ export const Aireador = ({ id, frecuencia }) => {
     return (
         <div className='col p-3 border-0'>
             <div className="text-start bg-dark text-center p-2 pt-5 pb-5 rounded-4 card-in press row mx-auto">
-                <h1 className="fs-4 fw-normal text-white">Airedor:</h1>
+                <h1 className="fs-4 fw-normal text-white">Aireador:</h1>
                 <h1 className="fs-5 fw-light text-white large-text">{id}</h1>
                 <h1 className="fs-5 fw-light text-secondary">Frecuencia: {frecuencia}</h1>
             </div>
